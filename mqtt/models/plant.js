d829b82b228362dd70c6142d9074533ba2ddcbb2
ttgray <ttgray@deakin.edu.au>
2020-09-08 20:49:01 +0930

    UPDATE: plant.js model update

--- mqtt/models/plant.js
+++ mqtt/models/plant.js
@@ -4,9 +4,9 @@ module.exports = mongoose.model('Plant', new mongoose.Schema(
     id: String,
     name: String,
     user: String,
-    temp: Float32Array,
-    light: Float32Array,
-    humidity: Float32Array,
-    moisure: Float32Array
+    temp: String,
+    light: String,
+    humidity: String,
+    moisure: String
 
 }));
